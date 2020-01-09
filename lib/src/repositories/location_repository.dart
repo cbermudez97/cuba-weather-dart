c6337e4bdf2a6106d532e232c99a1f598757277a
leynier <leynier41@gmail.com>
2020-01-09 16:32:47 -0500

    Implement Location repository

---
+++ lib/src/repositories/location_repository.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+
+import 'package:cuba_weather_dart/src/data_providers/data_providers.dart';
+
+class LocationRepository {
+  LocationRepository() {
+    locations.sort((a, b) => b.length - a.length);
+  }
+
+  Future<String> getLocation(String locationQuery) async {
+    var locationQueryLowerCase = locationQuery.toLowerCase();
+    for (var location in locations) {
+      if (location.toLowerCase().contains(locationQueryLowerCase)) {
+        return location;
+      }
+    }
+    throw Exception('$locationQuery not found in the database');
+  }
+}
