2fcc65c2349ca4862da6151e19e2c02c2a1f5a5d
leynier <leynier41@gmail.com>
2020-01-09 16:31:52 -0500

    Implement Weather api data provider

---
+++ lib/src/data_providers/weather_api_client.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+import 'package:cuba_weather_dart/src/models/models.dart';
+
+class WeatherApiClient {
+  static const baseUrl = 'https://www.redcuba.cu/api/weather_get_summary/';
+  final Client httpClient;
+
+  WeatherApiClient(this.httpClient) : assert(httpClient != null);
+
+  Future<WeatherModel> fetchWeather(String location) async {
+    final url = baseUrl + location;
+    final resp = await this.httpClient.get(url);
+    if (resp.statusCode != 200) {
+      throw Exception('error getting weather for location');
+    }
+    final json = jsonDecode(resp.body);
+    return WeatherModel.fromJson(json);
+  }
+}
