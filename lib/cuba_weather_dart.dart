27077368b5d960236ab9903bd272cd196bb75127
leynier <leynier41@gmail.com>
2020-01-09 16:34:09 -0500

    Export package and implement new test

--- lib/cuba_weather_dart.dart
+++ lib/cuba_weather_dart.dart
@@ -1,7 +1,3 @@
 library cuba_weather_dart;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:cuba_weather_dart/src/cuba_weather.dart';
