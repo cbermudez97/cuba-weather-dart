3d05e8a19542764f714625baeacb6799e2974528
leynier <leynier41@gmail.com>
2020-01-10 07:41:41 -0500

    Comment weather provider

--- lib/src/data_providers/weather_api_client.dart
+++ lib/src/data_providers/weather_api_client.dart
@@ -4,12 +4,15 @@ import 'package:http/http.dart';
 
 import 'package:cuba_weather_dart/src/models/models.dart';
 
+/// Class to provide the functionality of making API requests
 class WeatherApiClient {
   static const baseUrl = 'https://www.redcuba.cu/api/weather_get_summary/';
   final Client httpClient;
 
+  /// Class constructor
   WeatherApiClient(this.httpClient) : assert(httpClient != null);
 
+  /// Method to make the request GET to API
   Future<WeatherModel> fetchWeather(String location) async {
     final url = baseUrl + location;
     final resp = await httpClient.get(url);
