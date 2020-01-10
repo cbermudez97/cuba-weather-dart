eb3fe8c86babe00565d86a0814689fd8f1c2047e
leynier <leynier41@gmail.com>
2020-01-10 07:42:20 -0500

    Comment weather repository

--- lib/src/repositories/weather_repository.dart
+++ lib/src/repositories/weather_repository.dart
@@ -3,11 +3,14 @@ import 'dart:async';
 import 'package:cuba_weather_dart/src/data_providers/data_providers.dart';
 import 'package:cuba_weather_dart/src/models/models.dart';
 
+/// Class to provide the functionality of obtaining meteorological data
 class WeatherRepository {
   final WeatherApiClient weatherApiClient;
 
+  /// Class constructor
   WeatherRepository(this.weatherApiClient) : assert(weatherApiClient != null);
 
+  /// Method that given a location returns the meteorological information
   Future<WeatherModel> getWeather(String location) async {
     return await weatherApiClient.fetchWeather(location);
   }
