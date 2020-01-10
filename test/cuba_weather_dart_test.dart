fda44f2ae2724b6b0bd2092c09f87fdb42faeeac
leynier <leynier41@gmail.com>
2020-01-09 19:41:46 -0500

    Change test implementation

--- test/cuba_weather_dart_test.dart
+++ test/cuba_weather_dart_test.dart
@@ -3,8 +3,10 @@ import 'package:test/test.dart';
 import 'package:cuba_weather_dart/cuba_weather_dart.dart';
 
 void main() {
-  test('print Habana weather', () {
+  test('Print Habana weather', () {
     var cubaWeather = CubaWeather();
-    print(cubaWeather.getWeather('Habana'));
+    cubaWeather.getWeather('Habana').then((weather) {
+      print(weather.cityName);
+    });
   });
 }
