9e94a18ba5605a3591cfea8f6f24897fd7666287
leynier <leynier41@gmail.com>
2020-01-10 07:41:13 -0500

    Comment main class

--- lib/src/cuba_weather.dart
+++ lib/src/cuba_weather.dart
@@ -6,15 +6,19 @@ import 'package:cuba_weather_dart/src/data_providers/data_providers.dart';
 import 'package:cuba_weather_dart/src/models/models.dart';
 import 'package:cuba_weather_dart/src/repositories/repositories.dart';
 
+/// Main class to provide package functionality.
 class CubaWeather {
   LocationRepository _locationRepository;
   WeatherRepository _weatherRepository;
 
+  /// Class constructor
   CubaWeather() {
     _locationRepository = LocationRepository();
     _weatherRepository = WeatherRepository(WeatherApiClient(Client()));
   }
 
+  /// Method that given a location of the user searches the known locations to
+  /// find the best match and returns the meteorological information.
   Future<WeatherModel> getWeather(String location) async {
     var _location = await _locationRepository.getLocation(location);
     return await _weatherRepository.getWeather(_location);
