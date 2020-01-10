9c98d867a4fab92757a0c081af838c2715df6acb
leynier <leynier41@gmail.com>
2020-01-10 07:42:02 -0500

    Comment location repository

--- lib/src/repositories/location_repository.dart
+++ lib/src/repositories/location_repository.dart
@@ -2,11 +2,14 @@ import 'dart:async';
 
 import 'package:cuba_weather_dart/src/data_providers/data_providers.dart';
 
+/// Class to provide the functionality of searching for a location
 class LocationRepository {
   LocationRepository() {
     locations.sort((a, b) => a.length - b.length);
   }
 
+  /// Method that returns the best match of the given location with the known
+  /// locations
   Future<String> getLocation(String locationQuery) async {
     var locationQueryLowerCase = locationQuery.toLowerCase();
     for (var location in locations) {
