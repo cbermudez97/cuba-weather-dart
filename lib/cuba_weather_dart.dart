953126a3973086abfd8cdc1705078b51ed1ac126
leynier <leynier41@gmail.com>
2020-01-10 07:41:01 -0500

    Export repositories

--- lib/cuba_weather_dart.dart
+++ lib/cuba_weather_dart.dart
@@ -3,3 +3,4 @@ library cuba_weather_dart;
 export 'package:cuba_weather_dart/src/cuba_weather.dart';
 export 'package:cuba_weather_dart/src/data_providers/data_providers.dart';
 export 'package:cuba_weather_dart/src/models/models.dart';
+export 'package:cuba_weather_dart/src/repositories/repositories.dart';
