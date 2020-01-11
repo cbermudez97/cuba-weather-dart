27e65d5d031a99f70f161e6f86d7933c8c702d51
leynier <leynier41@gmail.com>
2020-01-10 19:25:36 -0500

    Improve location repository for use Levenshtein distance

--- lib/src/repositories/location_repository.dart
+++ lib/src/repositories/location_repository.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:edit_distance/edit_distance.dart';
+
 import 'package:cuba_weather_dart/src/data_providers/data_providers.dart';
 
 /// Class to provide the functionality of searching for a location
@@ -11,12 +13,23 @@ class LocationRepository {
   /// Method that returns the best match of the given location with the known
   /// locations
   Future<String> getLocation(String locationQuery) async {
-    var locationQueryLowerCase = locationQuery.toLowerCase();
+    locationQuery = locationQuery.toLowerCase();
     for (var location in locations) {
-      if (location.toLowerCase().contains(locationQueryLowerCase)) {
+      if (location.toLowerCase().contains(locationQuery)) {
         return location;
       }
     }
-    throw Exception('$locationQuery not found in the database');
+    var levenshtein = Levenshtein();
+    var best_location = locations[0].toLowerCase();
+    var best_distance = levenshtein.distance(locationQuery, best_location);
+    for (var i = 1; i < locations.length; ++i) {
+      var temp_location = locations[i].toLowerCase();
+      var temp_distance = levenshtein.distance(locationQuery, temp_location);
+      if (temp_distance < best_distance) {
+        best_location = temp_location;
+        best_distance = temp_distance;
+      }
+    }
+    return best_location;
   }
 }
